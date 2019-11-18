8c98a8675b602e95e920c185311821a2e0b9348d
blesswood <40756812+blesswood@users.noreply.github.com>
2019-11-18 21:55:51 +0300

    fixed performance

--- finda.py
+++ finda.py
@@ -6,6 +6,7 @@ import requests
 import sys
 from threading import Thread
 import os
+import time
 
 numline = 1
 isproxy = {"http":"0",
@@ -27,8 +28,7 @@ def helpmenu():
 	print("This is the active scanning, which may be forbidden in your country, please, use it to scan only your own server(s)\n")
 	print("Usage:")
 	print('finda.py -h[elp] -> this help')
-	print("finda.py URL [-s][ilent] [-f][ast] [-p][roxy] [ip:port]-> start scan")
-	print("\nI strongly recommend to use silent mode with '-f' option, because without it after successfull result may appear some extra wrong strings\n")
+	print("finda.py URL [-s][ilent] [-f][ast] [-p][roxy] [ip:port] -> scan options\n")
 	print("I want your feedback here:\n     Telegram: @kennnies \n     Jabber: explo1t@xmpp.jp")
 	print("\nThanks!")
 	sys.exit()
@@ -39,30 +39,19 @@ try:
 except IndexError:
 	helpmenu()
 
-option = ''
-try:
-	option = sys.argv[2]
-except IndexError:
-	pass
-
-fast = ''
-try:
-	fast = sys.argv[3]
-except IndexError:
-	pass
-
-t = False #sys.exit() if found result
+t = False #sys.exit() if found result(if True)
 
 def ifsilent(num): #scrollbar
-    res = '\r%i/7339' % num
-    sys.stdout.write(res)
-    sys.stdout.flush()
+	res = '\r%i/7339' % num #7339 - num of string in findadb
+	sys.stdout.flush()
+	sys.stdout.write(res)
 
 def getpage(file, sitename, header): #main func(active scan)
 	page = file.readline()
 	if not page: #if file isn't readable
 		print("Sorry, i couldn't read line from file findadb.py")
 		sys.exit()
+	global t
 	siteresult = sitename + '/' + page #get page address
 	if (isproxy["http"]!="0"):
 		try:
@@ -76,69 +65,94 @@ def getpage(file, sitename, header): #main func(active scan)
 	else:
 		response = requests.get(siteresult[:-1], headers=header)
 	if not response:
-		dat = response.url + ' is wrong!' #check all links(idk who wants it...)
-		if con == False:
-			print(dat)
-		else:
-			global numline
-			ifsilent(numline)#count number of current line in silent mode
-			numline+=1
+		if(t==False):
+			dat = response.url + ' is wrong!' #check all links(idk who wants to see it...)
+			if con == False:
+				print(dat)
+			else:
+				global numline
+				ifsilent(numline)#number of current line in silent mode
+				numline+=1
 	else:
 		print()
 		dat = str(response.url) + ' IS OK'
 		print(dat)
-		global t
 		t = True
 
 con = False
+isfast = False
 
-def silentmode(): #check for silent mode
+def mode(): #check for silent mode
 	for i in range(2,5):
 		try:
-			if ('-s' in sys.argv[i]) or ('-s' in sys.argv[i]):
+			if ('-s' in sys.argv[i]):
 				global con
 				con = True
-			elif('-p' in sys.argv[i]) or ('-p' in sys.argv[i]):
+			elif('-p' in sys.argv[i]):
 				global isproxy
 				isproxy["http"] = "http://" + str(sys.argv[i+1])
 				isproxy["https"] = "https://" + str(sys.argv[i+1])
+			elif('-f' in sys.argv[i]):
+				global isfast
+				isfast = True
 		except IndexError:
 			pass
 
 sitename = str(sitename)
+
 if "-h" in sitename:
 		helpmenu() #show help menu
 else:
 		sitename = 'http://' + sitename
 		file = open('/usr/local/bin/findadb.py')
 		header = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'}
-		silentmode()
+		mode()
 		with file as f:
-			while True: #start scanning
-				try:
-					if('-f' in option) or ('-f' in fast): #check for fast mode and create 5 threads if so
+			if(isfast==True):
+				while True: #start scanning
+					try: #check for fast mode and create 5 threads if so
 						th1 = Thread(target=getpage, args=(file,sitename,header,))
-						th2 = Thread(target=getpage, args=(file,sitename,header,))
-						th3 = Thread(target=getpage, args=(file,sitename,header,))
-						th4 = Thread(target=getpage, args=(file,sitename,header,))
-						th5 = Thread(target=getpage, args=(file,sitename,header,))
 						th1.start()
+						time.sleep(0.1)
+
+						th2 = Thread(target=getpage, args=(file,sitename,header,))
 						th2.start()
+						time.sleep(0.1)
+
+						th3 = Thread(target=getpage, args=(file,sitename,header,))
 						th3.start()
+						time.sleep(0.1)
+
+						th4 = Thread(target=getpage, args=(file,sitename,header,))
 						th4.start()
+						time.sleep(0.1)
+
+						th5 = Thread(target=getpage, args=(file,sitename,header,))
 						th5.start()
+						time.sleep(0.1)
+
 						th1.join()
 						th2.join()
 						th3.join()
 						th4.join()
 						th5.join()
-					else: #else start 1 thread
+						if (t==True):
+							sys.exit()
+					except KeyboardInterrupt: #in case of cancelling program
+						print('\nExiting')
+						time.sleep(2.5)
+						sys.exit()
+			else: #else start 1 thread
+				while True:
+					try:
 						th1 = Thread(target=getpage, args=(file,sitename,header,))
 						th1.start()
 						th1.join()
-				except KeyboardInterrupt: #in case of cancelling program
-					print('\nexiting')
-					sys.exit()
+						if(t==True):
+							sys.exit()
+					except KeyboardInterrupt: #in case of cancelling program
+						print('\nExiting')
+						sys.exit()
 				if t==True: #if success
 					sys.exit()
 			file.close()
