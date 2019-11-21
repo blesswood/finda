deb7e031eb78544359a030f0c7c5e20fc88737ce
blesswood <blesswood@bk.ru>
2019-11-21 12:35:04 +0300

    Add uninstaller

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import os
 r = "Do you really want to install this software? (y/n)"
 print(r, end=" ")
 if('y' in input()):
-	import os
 	pwd = str(os.popen('pwd').read().split())[2:-2]
 	try:
 		symlink = "sudo ln -s " + pwd +"/finda.py"+" /usr/local/bin"
@@ -21,5 +20,6 @@ if('y' in input()):
 #			os.system('sudo rm /usr/local/bin/cyth.py')
 #		os.system(symlink)
 		print('Success! Try: finda.py -h or man finda.py')
+		print('Uninstall: sudo python3 uninstall.py')
 	except SystemError:
 		print("Oops, try with sudo!")
---
+++ uninstall.py
@@ -0,0 +1,4 @@
+import os
+os.popen('sudo rm /usr/local/bin/finda*')
+os.popen('sudo rm /usr/share/man/man1/finda.py*')
+print('Success!')
