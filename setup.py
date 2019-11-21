23214b9c5ebdf70603be411bdbe7c0cbc6a0cff7
blesswood <blesswood@bk.ru>
2019-11-21 18:12:22 +0300

    fixed issues

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import os
 r = "Do you really want to install this software? (y/n)"
 print(r, end=" ")
 if('y' in input()):
-	pwd = str(os.popen('pwd').read().split())[2:-2]
+	pwd = str(os.popen('pwd').read().split())[2:-2] #get current directory
 	try:
-		symlink = "sudo ln -s " + pwd +"/finda.py"+" /usr/local/bin"
-		os.system('sudo chmod +x finda.py')
+		symlink = "sudo ln -s " + pwd +"/finda.py"+" /usr/local/bin" #symlink finda.py to /usr/local/bin
+		os.popen('sudo chmod +x finda.py') #give permission to be executable
 		os.popen(symlink)
-		symlink = "sudo ln -s " + pwd +"/findadb.py"+" /usr/local/bin"
+		symlink = "sudo ln -s " + pwd +"/findadb.py"+" /usr/local/bin" #symlink db
 		os.popen(symlink)
-		r = 'sudo cp findaman /usr/share/man/man1/finda.py.1'
+		r = 'sudo cp findaman /usr/share/man/man1/finda.py.1' #copy manpage to man1
 		os.popen(r)
 		r = 'sudo gzip /usr/share/man/man1/finda.py.1'
 		os.popen(r)
@@ -23,3 +23,5 @@ if('y' in input()):
 		print('Uninstall: sudo python3 uninstall.py')
 	except SystemError:
 		print("Oops, try with sudo!")
+	except KeyboardInterrupt:
+		sys.exit()
