7d711344ea965d0706d9d23fbe619b8051afae59
blesswood <blesswood@bk.ru>
2019-11-21 18:12:03 +0300

    Add comments

--- finda.py
+++ finda.py
@@ -35,13 +35,13 @@ def helpmenu():
 	print("\nThanks!")
 	sys.exit()
 
-sitename=''
+sitename = ''
 try:
 	sitename = sys.argv[1]
 except IndexError:
 	helpmenu()
 
-t = False #sys.exit() if found result(if True)
+ifresult = False #sys.exit() if found result(if True)
 
 def ifsilent(num): #scrollbar
 	res = '\r%i/7339' % num #7339 - num of string in findadb
@@ -57,7 +57,7 @@ def getpage(file, sitename, header, timestart): #main func(active scan)
 	if not page: #if file isn't readable
 		print("Sorry, i couldn't read line from file findadb.py")
 		sys.exit()
-	global t
+	global ifresult
 	siteresult = sitename + '/' + page #get page address
 	timeleft = time.time()
 	if (isproxy["http"]!="0"):
@@ -72,15 +72,16 @@ def getpage(file, sitename, header, timestart): #main func(active scan)
 	else:
 		response = requests.get(siteresult[:-1], headers=header)
 	if not response:
-		if(t==False):
+		if(ifresult==False):
 			dat = response.url + ' is wrong!' #check all links(idk who wants to see it...)
-			checktime(timeleft)
 			if con == False:
 				print(dat)
+				checktime(timeleft)
 			else:
 				global numline
 				ifsilent(numline)#number of current line in silent mode
 				numline+=1
+				checktime(timeleft) #timeleft stop and count
 	else:
 		print()
 		dat = str(response.url) + ' IS OK'
@@ -88,25 +89,25 @@ def getpage(file, sitename, header, timestart): #main func(active scan)
 		timespent = 'Time spent: ' + str(round(time.time()-timestart)) + 's\n'
 		sys.stdout.write(timespent)
 		sys.stdout.flush()
-		t = True
+		ifresult = True
 
-con = False
-isfast = False
+con = False #if silent mode is enabled, become True
+isfast = False #if fast mode is enabled, become True
 
-def mode(): #check for silent mode
+def mode(): #check for mode
 	for i in range(2,5):
 		try:
-			if ('-s' in sys.argv[i]):
+			if ('-s' in sys.argv[i]): # check for '-s' option (silentmode)
 				global con
 				con = True
-			elif('-p' in sys.argv[i]):
+			elif('-p' in sys.argv[i]): # check for '-p' option (proxy)
 				global isproxy
-				isproxy["http"] = "http://" + str(sys.argv[i+1])
+				isproxy["http"] = "http://" + str(sys.argv[i+1]) #write proxy(next option after '-p') to isproxy
 				isproxy["https"] = "https://" + str(sys.argv[i+1])
-			elif('-f' in sys.argv[i]):
+			elif('-f' in sys.argv[i]): #check for '-f' option (fastmode)
 				global isfast
 				isfast = True
-		except IndexError:
+		except IndexError: # if no options given
 			pass
 
 sitename = str(sitename)
@@ -114,31 +115,32 @@ header = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHT
 
 def checktime(timeleft):
 	global numpop
-	numpop = 450
+	numpop = 450 #number of popular admin pages in db
 	if(numpop-countline<=0):
-		numpop = 7339
+		numpop = 7339 #if first 450 pages isn't available
 	sys.stdout.flush()
-	res = '        time left: ' + str(round((time.time()-timeleft)*(numpop-countline))) + 's'
+	res = '        time left: ' + str(round((time.time()-timeleft)*(numpop-countline))) + 's ' #write time left
 	sys.stdout.write(res)
 
 def start():
 	global sitename
-	sitename = 'http://' + sitename
+	if not 'http://' in sitename:
+		sitename = 'http://' + sitename
 	file = open('/usr/local/bin/findadb.py')
 	if '-w' in sitename:
-		pass
+		pass #escape mode checking for wizard
 	else:
-		mode()
+		mode() #check options
 	with file as f:
 		if(isfast==True):
-			timestart = time.time()
+			timestart = time.time() #started time for counting timespent
 			while True: #start scanning
 				try: #check for fast mode and create 5 threads if so
-					timeleft = time.time()
+					timeleft = time.time() #timeleft start
 					th1 = Thread(target=getpage, args=(file,sitename,header,timestart,))
-					th1.start()
+					th1.start() #start thread with 4 args
 					time.sleep(0.05)
-					th1.join()
+					th1.join() #close thread
 
 					th2 = Thread(target=getpage, args=(file,sitename,header,timestart))
 					th2.start()
@@ -160,9 +162,9 @@ def start():
 					time.sleep(0.1)
 					th5.join()
 
-					if (t==True):
-						sys.exit()
-				except KeyboardInterrupt: #in case of cancelling program
+					if (ifresult==True):
+						sys.exit() #exit code
+				except KeyboardInterrupt: #in case of cancelling work by user
 					for i in range(4):
 						sys.stdout.flush()
 					time.sleep(1)
@@ -179,16 +181,16 @@ def start():
 					th1.start()
 					th1.join()
 
-					if(t==True):
+					if(ifresult==True):
 						sys.exit()
 
-				except KeyboardInterrupt: #in case of cancelling program
+				except KeyboardInterrupt: #in case of cancelling work
 					sys.stdout.flush()
 					time.sleep(1)
 					sys.stdout.write('\n')
 					sys.stdout.flush()
 					sys.exit()
-			if t==True: #if success
+			if ifresult==True: #if success
 				sys.exit()
 		file.close()
 
@@ -209,20 +211,20 @@ def startWizard():
 	print("Enter site URL:", end = " ")
 	global sitename
 	try:
-		sitename = input()
+		sitename = input() #enter link
 		print("Do you want fast scan?(y/N):", end = " ")
-		if 'y' in input():
+		if 'y' in input(): #if user enabled fastmode
 			global isfast
 			isfast = True
 		print("Do you want silent mode?(Y/n):", end = " ")
-		if 'y' in input():
+		if 'y' in input(): #if user enabled silentmode
 			global con
 			con = True
-		print("Do you want to use proxy?(Y/n):", end = " ")
-		if 'y' in input():
+		print("Do you want to use proxy(http(s))?(Y/n):", end = " ")
+		if 'y' in input(): #if user want to use proxy
 			print('Enter proxy(ip:port)', end = " ")
 			proxy = input()
-			isproxy["http"] = "http://" + str(proxy)
+			isproxy["http"] = "http://" + str(proxy) #write inputed proxy to isproxy
 			isproxy["https"] = "https://" + str(proxy)
 	except KeyboardInterrupt:
 		print()
@@ -231,7 +233,7 @@ def startWizard():
 
 if "-h" in sitename:
 	helpmenu() #show help menu
-elif "-w" in sitename:
+elif "-w" in sitename: #enable wizard
 	startWizard()
 else:
 	start()
