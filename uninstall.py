31662720e419f1dcaedbde4f4e97ecf4170de521
blesswood <blesswood@bk.ru>
2019-11-21 12:39:45 +0300

    Add confirmation

--- uninstall.py
+++ uninstall.py
@@ -1,4 +1,8 @@
 import os
-os.popen('sudo rm /usr/local/bin/finda*')
-os.popen('sudo rm /usr/share/man/man1/finda.py*')
-print('Success!')
+print('Are you sure want to uninstall this software?(y/N) ', end = " ")
+if 'y' in input():
+    os.popen('sudo rm /usr/local/bin/finda*')
+    os.popen('sudo rm /usr/share/man/man1/finda.py*')
+    print('Success!')
+else:
+    print('Aborted')
